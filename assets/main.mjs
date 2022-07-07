e94b8e5a058ee6507f82dc463f637ddb8edf3106
Brenorml <breno.lucena@gmail.com>
2022-07-07 15:45:59 +0100

    Exercitando import e export no JavaScript

---
+++ assets/main.mjs
@@ -0,0 +1,6 @@
+import { mostraIdade, mostraCidade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("Breno", 39));
+console.log(mostraCidade("Breno", "Recife"));
+console.log(mostraHobby("Breno", "jogar PC games"));
+
